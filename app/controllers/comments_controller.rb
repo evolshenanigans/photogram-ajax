299abd0087016fb8a756012e362035c4b9fa354d
Gustavo Joshua Gutierrez <51761125+evolshenanigans@users.noreply.github.com>
2024-02-29 19:00:48 +0000

    Revert "added create,update,edit,destroy ajax"
    This reverts commit 13cbf2ab8eae58a1bfce591592a9b6c874f05a87.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,7 +21,6 @@ class CommentsController < ApplicationController
   def edit
     respond_to do |format|
       format.html
-      format.js
     end
   end
 
@@ -34,7 +33,6 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_back fallback_location: root_path, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
-        format.js
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -48,7 +46,6 @@ class CommentsController < ApplicationController
       if @comment.update(comment_params)
         format.html { redirect_to root_url, notice: "Comment was successfully updated." }
         format.json { render :show, status: :ok, location: @comment }
-        format.js
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -62,10 +59,6 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
-
-      format.js do
-        render template: "comments/destroy"
-      end
     end
   end
 
